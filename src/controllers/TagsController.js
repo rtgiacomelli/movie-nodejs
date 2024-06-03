43c4142a3099e44ea1bd40278f8de2e576dc5189
Rodolfo <rortimoteo@gmail.com>
2024-06-03 17:59:56 -0300

    commit 03/06

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -1,71 +1,8 @@
-// const knex = require("../database/knex");
-
-// class TagsController {
-//   async index(request, response) {
-//     const { user_id } = request.params;
-
-//     const tags = await knex("tags")
-//       .where({ user_id })
-
-//     return response.json(tags);
-//   }
-
-//   async create(request, response){
-//     try {
-//       const { name } = request.body;
-//       const { note_id } = request.params;
-//       const { user_id } = request.params;
-      
-//       const [tag_id] = await knex("tags").insert({
-//         note_id,
-//         user_id,
-//         name
-//       });
-      
-//       response.status(201).json({ tag_id });
-//     } catch (error) {
-//       response.status(500).json({ error: error.message });
-//     }
-//   }
-
-//   async show(request, response) {
-//     try {
-//       const { id } = request.params;
-
-//       const tag = await knex("tags").where({ id }).first();
-
-//       if (!tag) {
-//         return response.status(404).json({ error: 'Tag not found' });
-//       }
-
-//       return response.json(tag);
-//     } catch (error) {
-//       response.status(500).json({ error: error.message });
-//     }
-//   }
-
-//   async delete(request, response) {
-//     try {
-//       const { id } = request.params;
-
-//       await knex("tags").where({ id }).delete();
-
-//       response.json({ message: 'Tag deleted successfully' });
-//     } catch (error) {
-//       response.status(500).json({ error: error.message });
-//     }
-//   }
-
-//   }
-
-
-// module.exports = TagsController;
-
 const knex = require("../database/knex");
 
 class TagsController {
   async index(request, response) {
-    const { user_id } = request.params;
+    const user_id = request.user.id;
 
     const tags = await knex("tags")
       .where({ user_id })
--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,13 +3,6 @@ const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  /**
-   * index - GET para listar vários registros.
-   * show - GET para exibir um registro específico.
-   * create - POST para criar um registro.
-   * update - PUT para atualizar um registro.
-   * delete - DELETE para remover um registro.
-   */
 
   async create(request, response) {
     const { name, email, password } = request.body;
@@ -32,10 +25,10 @@ class UsersController {
 
   async update(request, response) {
     const { name, email, password, old_password } = request.body;
-    const { id } = request.params;
+    const user_id = request.user.id;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id]);
 
     if(!user) {
       throw new AppError("Usuário não encontrado.");
@@ -72,7 +65,7 @@ class UsersController {
     password = ?,
     updated_at = DATETIME('now')
     WHERE id = ?`,
-    [user.name, user.email, user.password, id]);
+    [user.name, user.email, user.password, user_id]);
 
     return response.status(200).json();
   }
